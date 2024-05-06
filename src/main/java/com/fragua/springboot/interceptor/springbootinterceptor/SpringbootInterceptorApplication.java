90071d4445aba285aa8cdf654350b0a0dd901ae3
Jaime Suarez Espinoza <jaime_suarez@fragua.com.mx>
2024-05-06 15:13:27 -0600

    first commit

---
+++ src/main/java/com/fragua/springboot/interceptor/springbootinterceptor/SpringbootInterceptorApplication.java
@@ -0,0 +1,13 @@
+package com.fragua.springboot.interceptor.springbootinterceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInterceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootInterceptorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fragua/springboot/interceptor/springbootinterceptor/SpringbootInterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fragua.springboot.interceptor.springbootinterceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInterceptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
